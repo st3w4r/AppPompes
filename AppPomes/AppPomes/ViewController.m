0da8ea9745796d6a5f3bff7de5e1f09c4be74ba5
St3w4r <st3w4r@gmail.com>
2013-04-25 14:32:08 +0200

    Déplacement des boutons dans la vue.

--- AppPomes/AppPomes/ViewController.m
+++ AppPomes/AppPomes/ViewController.m
@@ -9,10 +9,15 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+    
 @end
 
+
 @implementation ViewController
+
+UIButton *btnCommencer;
+UIButton *btnReglage;
+UIButton *btnStats;
 
 - (void)viewDidLoad
 {
@@ -21,8 +26,51 @@
     NSString *msg = @"Hello World";
     NSLog(@"%@",msg);
     
+    //Bouton commencer
+
+    btnCommencer = [UIButton buttonWithType:UIButtonTypeCustom];
+    btnCommencer.frame = CGRectMake(0.0, 0.0, self.view.frame.size.width, self.view.frame.size.height/2);
+    [btnCommencer setBackgroundColor:[UIColor colorWithRed:0.0 green:1.0 blue:0.4 alpha:1.0]];
+    [btnCommencer setTitle:@"Commencer" forState:UIControlStateNormal];
+    [btnCommencer addTarget:self action:@selector(btnHandleMethode:) forControlEvents:UIControlEventTouchUpInside];
+    
+    //Bouton Réglage
+    btnReglage = [UIButton buttonWithType:UIButtonTypeCustom];
+    btnReglage.frame = CGRectMake(0.0, btnCommencer.frame.size.height, self.view.frame.size.width, (self.view.frame.size.height/10));
+    [btnReglage setBackgroundColor:[UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:1.0]];
+    [btnReglage setTitle:@"Réglages" forState:UIControlStateNormal];
+    [btnReglage addTarget:self action:@selector(btnHandleMethode:) forControlEvents:UIControlEventTouchUpInside];
+    
+    //Bouton stat
+    btnStats = [UIButton buttonWithType:UIButtonTypeCustom];
+    btnStats.frame = CGRectMake(0.0, btnCommencer.frame.size.height + btnReglage.frame.size.height, self.view.frame.size.width, (self.view.frame.size.height - (btnCommencer.frame.size.height + btnReglage.frame.size.height)));
+    [btnStats setBackgroundColor:[UIColor colorWithRed:0.1 green:1.0 blue:1.0 alpha:1.0]];
+    [btnStats setTitle:@"Statistiques" forState:UIControlStateNormal];
+
+    [btnStats addTarget:self action:@selector(btnHandleMethode:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:btnCommencer];
+    [self.view addSubview:btnReglage];
+    [self.view addSubview:btnStats];
 }
 
+- (void)btnHandleMethode:(UIButton*)sender
+{
+//    UIButton *button = (UIButton*)sender;
+    [sender setSelected:![sender isSelected]];
+//    [button setHighlighted:![button isHighlighted]];
+    if (sender.selected == true)
+    {
+        [sender setAlpha:0.6];
+    }
+    else
+    {
+        [sender setAlpha:1.0];
+    }
+    
+    NSLog(@"%@",sender.titleLabel.text);
+    
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
